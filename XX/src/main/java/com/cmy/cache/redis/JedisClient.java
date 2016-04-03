a8bcacd3a59901b74b346be5878a8df0022efcea
kevin72c <512523616@qq.com>
2016-04-03 22:50:33 +0800

    oom

---
+++ XX/src/main/java/com/cmy/cache/redis/JedisClient.java
@@ -0,0 +1,27 @@
+package com.cmy.cache.redis;
+
+import java.util.Map;
+
+import redis.clients.jedis.Jedis;
+
+public class JedisClient {
+
+    static Jedis jedis;
+    static {
+        jedis = new Jedis("127.0.0.1", 6380);
+//        jedis.auth("");
+    }
+    
+    /**
+     * 获取字典
+     * 
+     * @param jedisEnum
+     * @param dictName
+     * @return
+     */
+    public Map<String, String> getDictionary(String dictName) {
+        Map<String, String> dict = jedis.hgetAll(JedisEnum.dict + dictName);
+        return dict;
+    }
+
+}
