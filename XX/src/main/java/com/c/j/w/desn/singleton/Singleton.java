0d0cd5f0baa08eec1e343af5c85f4cb1c81fb632
kevin72c <512523616@qq.com>
2017-01-28 17:37:21 +0800

    .

---
+++ XX/src/main/java/com/c/j/w/desn/singleton/Singleton.java
@@ -0,0 +1,19 @@
+package com.c.j.w.desn.singleton;
+
+public class Singleton {
+
+    public static Singleton instance = null;
+
+    private Singleton() {}
+
+    public static Singleton getInstance() {
+        if (instance == null) {
+            synchronized (instance) {
+                if (instance == null) {
+                    instance = new Singleton();
+                }
+            }
+        }
+        return instance;
+    }
+}
