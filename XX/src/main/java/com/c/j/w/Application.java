d2ec533bc0d449a23c171365455d70f526077155
Kevin <512523616@qq.com>
2017-09-10 21:18:04 +0800

    .

--- XX/src/main/java/com/c/j/w/Application.java
+++ XX/src/main/java/com/c/j/w/Application.java
@@ -1,4 +1,4 @@
-package com.application;
+package com.c.j.w;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ XX/src/main/java/com/c/j/w/lambda/LambdaTest.java
@@ -0,0 +1,12 @@
+package com.c.j.w.lambda;
+
+/**
+ * @Author: chenjw
+ * @Date: 2017/9/10 下午8:48
+ * @Description: xxx
+ */
+public class LambdaTest {
+    public static void main(String[] args) {
+
+    }
+}
